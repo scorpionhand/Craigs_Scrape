cfeabb1ec90be0ab279918d9cbbdf77601c35e54
dsfackrell <84728935+dsfackrell@users.noreply.github.com>
2024-06-10 19:11:00 -0500

    added Schema

---
+++ schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Craigs_Scrap(
+    id SERIAL PRIMARY KEY,
+    List_Type VARCHAR(30),
+    Price VARCHAR(30),
+    Item_Title VARCHAR,
+    Time_Listed VARCHAR(30),
+    Location VARCHAR(30),
+);
